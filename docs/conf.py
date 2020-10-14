76f7078638f270e4e43e3ab36ba6038e5101cf43
Zoltan Fedor <zoltan.1.fedor@gmail.com>
2020-10-14 16:27:22 -0400

    Falcon-Limiter 0.1.1 -> 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = '2020, Zoltan Fedor'
 author = 'Zoltan Fedor'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
